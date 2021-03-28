c0709087eb341d2a62dbea7f68322a417cf9a586
Jinwoo Kang <jikang@c6r10s6.42seoul.kr>
2021-03-28 17:03:21 +0900

    back up

---
+++ C01/ex07/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+int		main(int argc, char **argv)
+{
+	std::ifstream	ifs;
+	std::ofstream	ofs;
+	std::string		file;
+	std::string		line;
+	std::string		s1;
+	std::string		s2;
+	std::string::size_type pos;
+
+	if (argc !=4)  // 인자로 파일이름, 문자열 두개가 들어오지 않으면
+	{
+		std::cout<<"Error : wrong argument\n";
+		return (-1);
+	}
+	file = argv[1];  //파일 이름
+	s1=argv[2];  // 첫번째 문자열
+	s2=argv[3];  // 두번째 문자열
+	ifs.open(file);  // 읽을 파일 열기
+	file += ".replace";  // 파일이름 수정
+	ofs.open(file);  // 출력할 파일 열기
+	if (!ifs.is_open() || !ofs.is_open())
+	{
+		std::cout << "Error : wrong file name\n";
+		return (-1);
+	}
+	while (getline(ifs, line))
+	{
+		pos = 0;
+		while (true)
+		{
+			pos = line.find(s1, pos);
+			if (pos == std::string::npos)
+				break ;
+
+		}
+	}
---
+++ C01/ex07/ofopen/test.cpp
@@ -0,0 +1,20 @@
+/*
+ * ofs.open 을 하면, ofs 가 그 자체로 그 파일이 되는 것같다.
+ * 파일 디스크립터의 역할과 스트링의 역할
+ * 그래서 ofs 에 string 을 넣어주고 close 하면, 
+ * 그 파일이 만들어진다.
+ *
+ * ofstream::out 은 stream 에 output operations 를 허용한다.
+ * ofstream::app 은 각 출력 작업 전에, 위치 표시기를 stream 의 끝으로 설정
+ */
+
+#include <fstream>
+
+int main()
+{
+	std::ofstream ofs;
+	ofs.open("test.txt", std::ofstream::out | std::ofstream::app);
+	ofs << " more lorem ipsum";
+	ofs.close();
+	return (0);
+}

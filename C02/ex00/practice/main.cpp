23465ef2f5fbe964d70e14563d6cc049dce5bdc2
Jinwoo Kang <jikang@c6r10s6.42seoul.kr>
2021-03-31 11:52:38 +0900

    uncomment

--- C02/ex00/practice/main.cpp
+++ C02/ex00/practice/main.cpp
@@ -29,13 +29,13 @@
 int main(void)
 {
 	Fixed a;
-	//Fixed b(a);
+	Fixed b(a);
 	Fixed c;
 
 	c = a;
 
-//	std::cout << a.getRawBits() << std::endl;
-//	std::cout << b.getRawBits() << std::endl;
-//	std::cout << c.getRawBits() << std::endl;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
 	return (0);
 }
